b592bd6166436d9ddb4ef4e04dfd8f8b1b6555c4
jiacheng1996 <53596227+jiacheng1996@users.noreply.github.com>
2020-11-22 17:49:31 +0800

    version 1.3

--- StructsToLogin/src/login1/LoginAuth.java
+++ StructsToLogin/src/login1/LoginAuth.java
@@ -1,31 +1,36 @@
 package login1;
 
+import java.io.InputStream;
 import java.sql.Connection;  
 import java.sql.DriverManager;  
 import java.sql.PreparedStatement;  
-import java.sql.ResultSet;  
+import java.sql.ResultSet;
+import java.util.Properties;
 
 public class LoginAuth {
 
 	public static boolean validate(String username, String password){  
 		 
 		boolean loginSuccess = false;
-		 
+		  InputStream input = null;
+		  Properties prop = new Properties();
 		  try {  
 		   // Load the mySQL driver
+		   input =  LoginAuth.class.getClassLoader().getResourceAsStream("db.properties");
+		   prop.load(input);
 		   Class.forName("com.mysql.cj.jdbc.Driver");
 		   
 		   System.out.println("Driver loaded");
 		   
 		   // Attempt to connect to database
 		   Connection connect = DriverManager.getConnection(  
-		   "jdbc:mysql://127.0.0.1:3306/", "root", "root");  
+		   "jdbc:mysql://127.0.0.1:3306/"+ prop.getProperty("dbName")+"?serverTimezone=UTC", prop.getProperty("dbUser"), prop.getProperty("dbPassword"));   
 		   
 		   System.out.println("Database connected");
 		   
 		   // Setup mySQL authentication query
 		   PreparedStatement statement = connect.prepareStatement(  
-		     "SELECT * FROM websiteuser.user WHERE username = ? AND password = ?");
+		     "select * FROM users where user_name=? and password=?");
 		   
 		   statement.setString(1, username);
 		   statement.setString(2, password);
